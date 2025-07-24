986fae5d3be66f8d29b292b5e5af1c0f3187e040
Devarshan-0 <sraightlines6533@gmail.com>
2025-07-25 03:31:32 +0530

    Update mcct_app.py

--- mcct_app.py
+++ mcct_app.py
@@ -483,7 +483,7 @@ if run_sim:
     st.markdown(f"### 🌿 Environmental Data — Context: {selected_context}")
 
     for pi, plant in enumerate(plants):
-        data = plant_data[plant][selected_time][selected_context]
+        data = plant_data[plant][selected_context]
         st.markdown(
         f"**{plant}**: Temperature = {data['temperature']}°C, "
         f"Humidity = {data['humidity']}%, Soil pH = {data['soil_pH']}"
